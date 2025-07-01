9540f5822023ba1b5eb71faded71a976ba0b5e0f
Dysthesis <antheoraviel@protonmail.com>
2025-07-01 22:16:10 +1000

    Some more dumb optimisations

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -8,6 +8,10 @@ pub fn similarity(first: &str, second: &str) -> f64 {
     if first.is_empty() || second.is_empty() {
         return 0.0;
     }
+
+    if first == second {
+        return 1.0;
+    }
     let docs = [first, second];
     let tf_idf = TfIdf::from(docs.as_slice());
     let sim = Cosine::from(tf_idf.get().as_slice());
