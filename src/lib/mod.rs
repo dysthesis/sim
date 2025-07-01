bcbbd228ba91a46dc6ab8a7d6c7677db04343ad0
Dysthesis <antheoraviel@protonmail.com>
2025-07-01 21:37:11 +1000

    Small optimisation

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -5,6 +5,9 @@ pub mod cosine;
 pub mod tf_idf;
 
 pub fn similarity(first: &str, second: &str) -> f64 {
+    if first.is_empty() || second.is_empty() {
+        return 0.0;
+    }
     let docs = [first, second];
     let tf_idf = TfIdf::from(docs.as_slice());
     let sim = Cosine::from(tf_idf.get().as_slice());
